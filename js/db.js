8bfc6e4c58f0aa581e2f6d74c1cfedec3fc8437d
harolandresmechanzegarra09-wq <harolandresmechanzegarra09@gmail.com>
2026-03-02 16:15:30 -0500

    Update user role from 'vendedor' to 'trabajador'

--- js/db.js
+++ js/db.js
@@ -3,7 +3,7 @@ let db = {
     ventas: JSON.parse(localStorage.getItem("ventas")) || [],
     usuarios: JSON.parse(localStorage.getItem("usuarios")) || [
         { user: "admin", pass: "1234", rol: "admin" },
-        { user: "vendedor", pass: "1234", rol: "vendedor" }
+        { user: "trabajador", pass: "1234", rol: "vendedor" }
     ]
 };
 
@@ -11,4 +11,5 @@ function guardarDB(){
     localStorage.setItem("productos", JSON.stringify(db.productos));
     localStorage.setItem("ventas", JSON.stringify(db.ventas));
     localStorage.setItem("usuarios", JSON.stringify(db.usuarios));
+
 }
